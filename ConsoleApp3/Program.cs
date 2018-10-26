ca5d72e7a7e8c215a9beb5c479455502fb562082
Anoo <naray@LAPTOP-BLF2U6N6>
2018-10-26 19:21:51 -0400

    Added the code

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+
+namespace Assignment1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Runtime  Input         
+            /*Console.WriteLine("Please enter the Input: For EX 1+2+3");
+            string str = Console.ReadLine();
+            String[] s = str.Split('+');
+            int a =0;         
+            for(int i=0;i<s.Length;i++)
+            { 
+            a += int.Parse(s[i]);        
+            }            
+            Console.WriteLine(a);           
+            Console.ReadLine();*/
+            //Static Input
+            string str = "1+2+3";
+            String[] s = str.Split('+');
+            int a=0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                a += int.Parse(s[i]);
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
         }
     }
 }
